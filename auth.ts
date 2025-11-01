59607f4b2cc4e374317b7a1ad37c82dad3cec599
Michel Marion <kangkm19@gmail.com>
2025-11-01 14:49:59 +0000

    Just because

--- auth.ts
+++ auth.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth/next"
 import { authConfig } from "./auth.config"
- 
-export const auth = NextAuth(authConfig as Record<string, unknown>);
+
+// Type assertion to bypass NextAuth v4 type checking issues
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const auth = NextAuth(authConfig as any);
