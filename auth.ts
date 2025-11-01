21562a675b2a5a92f541d006f90c29f08d9feb6e
Michel Marion <kangkm19@gmail.com>
2025-11-01 14:41:52 +0000

    Fixed auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth/next"
 import { authConfig } from "./auth.config"
  
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-})
+export const auth = NextAuth(authConfig);
