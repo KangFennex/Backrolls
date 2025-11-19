635cf3cafbd4b3562db4b9d70fc17921cd2ea2eb
Michel Marion <kangkm19@gmail.com>
2025-11-19 13:50:56 +0000

    Removed extra imports

--- app/(pages)/series/page.tsx
+++ app/(pages)/series/page.tsx
@@ -1,7 +1,5 @@
 import PageContainer from '../../ui/pageContainer';
 import SeriesPageClient from '../../ui/series/SeriesPageClient';
-import { getFilteredQuotes } from '../../api/data/data';
-import { SeriesPageProps } from '../../lib/definitions';
 
 export default async function SeriesRoute() {
     return (
