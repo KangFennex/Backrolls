1db904669668cb8ff7ea745637417c7232ea6771
Michel Marion <kangkm19@gmail.com>
2025-11-23 13:15:14 +0000

    Fixed import submit page

--- app/(pages)/submit/page.tsx
+++ app/(pages)/submit/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import PageContainer from "../../ui/pageContainer";
-import SubmitPageClient from "../../ui/submit/submitPageClient";
+import SubmitPageClient from "../../ui/submit/SubmitPageClient";
 
 export default function Page() {
     return (
